afe6b03ac9ffe890c0ce958c87da8cbbe6393a3a
App Academy Student <student@appacademy.io>
2017-01-26 15:52:18 -0800

    Add render method

--- tile.rb
+++ tile.rb
@@ -2,6 +2,7 @@ class Tile
   def initialize
     @is_bomb = false
     @is_revealed = false
+    @neighbor_count = 0
   end
 
   def bomb?
@@ -18,5 +19,14 @@ class Tile
 
   def reveal
     @is_revealed = true
+  end
+
+  def set_neighbor_count(count)
+    @neighbor_count = count
+  end
+
+  def render
+    show = @is_bomb ? 'B' : @neighbor_count
+    @is_revealed ? show : ' '
   end
 end
